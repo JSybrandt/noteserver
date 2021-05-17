b6cd47aba0f2dd425bc4d954cb24aa813f0ce39b
Justin Sybrandt <jsybrandt@pop-os.localdomain>
2021-05-17 00:22:35 -0400

    Adds LSP message creation and parsing.

--- noteserver/server.py
+++ noteserver/server.py
@@ -4,31 +4,14 @@ This module contains create_flask_server, which is used to produce a flask app
 capable of responding to all required LSP and browser requests.
 """
 
-import flask
-from jsonrpc.backend import flask as jsonrpc_flask_backend
+import json
+from typing import Dict, Any, Tuple
 
 # Represents the RPC parameters of a json rpc message.
 Parameters = Dict[str, Any]
 
-def getJsonRpcMessage(method:str, params:Parameters)->bytes:
-  """Creates a JsonRpc message string.
 
-  Args:
-    method: The RPC method to call.
-    params: The parameters of the specified RPC message.
-
-  Returns:
-    A serialized json object in UTF-8 that is compatible with the jsonrpc
-    specification, containing the specified method name and parameters.
-  """
-  return json.dumps({
-      "jsonrpc": "2.0",
-      "id": 1,
-      "method": method,
-      "params": params,
-  }).encode("utf-8")
-
-def getLspMessage(method: str, params:Parameters)->bytes:
+def getLspMessage(method: str, params: Parameters) -> bytes:
   """Creates a LSP message with header and content components.
 
   Args:
@@ -40,15 +23,21 @@ def getLspMessage(method: str, params:Parameters)->bytes:
     section. The header describes the content, while the content itself is a
     utf-8 encoded json-rpc message.
   """
-  content = getJsonRpcMessage(method, params)
+  content = json.dumps({
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": method,
+      "params": params,
+  }).encode("utf-8")
   # Each header parameter is terminated by \r\n, and the header itself is also
   # terminated by \r\n.
-  header = f"Content-Length: {len(content)}\r\n"
-           "Content-Type: application/vscode-jsonrpc;charset=utf-8\r\n"
-           "\r\n"
+  header = (f"Content-Length: {len(content)}\r\n"
+            "Content-Type: application/vscode-jsonrpc;charset=utf-8\r\n"
+            "\r\n")
   return header.encode("utf-8") + content
 
-def parseLspMessage(message:bytes)->Tuple[str, Parameters]:
+
+def parseLspMessage(message: bytes) -> Tuple[str, Parameters]:
   """Extracts the method name and rpc parameters of a given LSP message.
 
   Args:
@@ -56,33 +45,16 @@ def parseLspMessage(message:bytes)->Tuple[str, Parameters]:
 
   Returns:
     A tuple of [method, parameters], that describes the RPC.
+
+  Raises:
+    ValueError: The message cannot be parsed.
   """
-  tokens = mesasge.decode("utf-8").split("\r\n\r\n")
+  tokens = message.decode("utf-8").split("\r\n\r\n")
   if len(tokens) != 2:
     raise ValueError(r"Serialized LSP Message does not contain \r\n\r\n.")
-  json.tokens[1]
-
-
-
-def create_flask_server(name:str=__name__, is_testing:bool = False)->flask.Flask:
-  """Creates the Flask webserver.
-
-  Args:
-    name: The applications module or package. Defaults to __name__.
-    is_testing: Sets the value on the returned Flask apps config["TESTING"]
-      parameter.
-  Returns:
-    A flask app that specifies endpoints for the LSP protocol, as well as for
-    generating webpages.
-  """
-  app = Flask(__name__)
-  app.config["TESTING"] = is_testing
-  app.register_blueprint(jsonrpc_flask_backend.api.as_blueprint())
-
-  @app.route("/echo", methods=["POST"])
-  def echo():
-    """Echos the
-    return "Hello world!"
-
-  return app
-
+  content = json.loads(tokens[1])
+  if "method" not in content:
+    raise ValueError("LSP Message does not contain 'method' key.")
+  if "params" not in content:
+    raise ValueError("LSP Message does not contain 'params' key.")
+  return content["method"], content["params"]
--- noteserver/server_test.py
+++ noteserver/server_test.py
@@ -1,19 +1,41 @@
 """Tests for the flask server."""
 
-from noteserver import server
-
 import json
 import unittest
-from typing import *
+from noteserver import server
 
 
-class FlaskServerTest(unittest.TestCase):
-  """Test for the flask server."""
+class LSPMessageTest(unittest.TestCase):
+  """Tests the creation and parsing of LSP RPCs."""
 
-  def setUp(self):
-    self.flask_app = server.create_flask_server(name="test_server", is_testing=True)
-    self.client = self.flask_app.test_client()
+  def test_encode_decode(self):
+    """Tests that an encoded message can be decoded."""
+    expected_method = "test/method"
+    expected_params = {
+        "str_param": "foo",
+        "int_param": 1,
+        "float_param": 2.1,
+    }
+    actual_method, actual_params = server.parseLspMessage(
+        server.getLspMessage(expected_method, expected_params))
+    self.assertEqual(actual_method, expected_method)
+    self.assertEqual(actual_params, expected_params)
 
-
-  def test_hello_world(self):
-    self.client.post("/", data=self.RE)
+  def test_encode_lsp_message(self):
+    """Tests that an encoded message matches an expected format."""
+    lsp_msg = server.getLspMessage("test_method", {"param": "val"})
+    tokens = lsp_msg.decode("utf-8").split("\r\n\r\n")
+    self.assertEqual(len(tokens), 2)
+    header, content = tokens
+    expected_header = ("Content-Length: 80\r\n"
+                       "Content-Type: application/vscode-jsonrpc;charset=utf-8")
+    self.assertEqual(header, expected_header)
+    self.assertEqual(
+        json.loads(content), {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "test_method",
+            "params": {
+                "param": "val"
+            }
+        })
