b30df5ff75662a06dbbb2253d37b78debdd643db
Justin Sybrandt <jsybrandt@pop-os.localdomain>
2021-08-01 20:09:54 -0400

    Refactors test vim

--- vim_test_env/launch_vim.sh
+++ vim_test_env/launch_vim.sh
@@ -4,10 +4,13 @@
 # .../noteserver/vim_test_env.
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-VIMRC="$SCRIPT_DIR/vimrc"
-VIM_LSP_PLUG_DIR="$SCRIPT_DIR/bundle/vim_lsp"
 
-# Vim runtimepath is comma-separated.
-RUNTIME_PATH="$VIMRC,$VIM_LSP_PLUG_DIR"
+RUNTIME_PATH="$SCRIPT_DIR"
+RUNTIME_PATH="$RUNTIME_PATH,$SCRIPT_DIR/vimrc"
 
-vim -c "set rtp=$RUNTIME_PATH" -u "$VIMRC"
+# Go to the script dir so that all of the vimrc's file paths can be relative to
+# the install directory.
+cd $SCRIPT_DIR
+
+
+ vim -c "set rtp=$RUNTIME_PATH" -u "$SCRIPT_DIR/vimrc" "$@"
